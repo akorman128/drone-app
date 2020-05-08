02b69f955ac0686ee0c509e44d6826dfaba1c5f4
Katie Helman <katherine.e.helman@vanderbilt.edu>
2020-05-08 16:59:59 -0400

    Pulled changes from Alex

--- website/render/js/login.js
+++ website/render/js/login.js
@@ -1,4 +1,41 @@
-// const auth = firebase.auth();
-// auth.signInWithEmailAndPassword(email, pass);
-// auth.createUserWithEmailAndPassword(email, pass);
-// auth.onAuthStateChange(firebaseUser => {});
+
+
+(function() {
+
+
+    const emailTxt = document.getElementById('email');
+    const password = document.getElementById('password');
+    const login = document.getElementById('login');
+    const signUp = document.getElementById('signUp');
+
+
+    //add login event
+    login.addEventListener('click', e => {
+        //get email, password
+        const email = emailTxt.value;
+        const pass = password.value;
+        const auth = firebase.auth();
+        //sign in
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch( e => console.log(e.message));
+    });
+
+    signUp.addEventListener('click', e =>{
+        const email = emailTxt.value;
+        const pass = password.value;
+        const auth = firebase.auth();
+
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch( e => console.log(e.message));
+    });
+
+    //realtime auth listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser){
+            console.log(firebaseUser);
+        } else{
+            console.log('Not logged in.');
+        }
+    });
+
+}());
